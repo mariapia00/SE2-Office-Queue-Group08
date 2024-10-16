e5acab29ef296ad93b8b79ddc371f5ad5308f4d4
RUGGERO ROSSINO s323698 <s323698@studenti.polito.it>
2024-10-16 19:10:17 +0200

    FIX: fixed cors settings

---
+++ backend/src/main/java/it/polito/queuemanagementsystem/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package it.polito.queuemanagementsystem.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("https://fowl-light-macaque.ngrok-free.app")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
