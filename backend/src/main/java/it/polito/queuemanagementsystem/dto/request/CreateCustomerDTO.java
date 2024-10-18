56da1a17f71af6470184a7e003a3ca0104a5c6dc
Nassim MANSOUR <mansournassim7@gmail.com>
2024-10-18 09:21:45 +0200

    Cleaning project, removing unused files

--- backend/src/main/java/it/polito/queuemanagementsystem/dto/request/CreateCustomerDTO.java
+++
@@ -1,9 +0,0 @@
-package it.polito.queuemanagementsystem.dto.request;
-
-import it.polito.queuemanagementsystem.model.Customer;
-
-public record CreateCustomerDTO(String name, String email, Integer age) {
-    public Customer toBo() {
-        return new Customer(null, this.name, this.email, this.age);
-    }
-}
--- backend/src/main/java/it/polito/queuemanagementsystem/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package it.polito.queuemanagementsystem.repository;
-
-import it.polito.queuemanagementsystem.model.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepository extends JpaRepository<Customer, Integer> {
-}
